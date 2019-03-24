3adbf2044cd2642b98bdec1dfac58c58ac13848a
zheusx360 <zheusx360@gmail.com>
2019-03-24 15:23:56 -0300

    Criação das classes e atualização da base de dados

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,4 +1,9 @@
 ﻿
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+
 namespace SalesWebMvc.Models
 {
     public class Department
@@ -6,5 +11,25 @@ namespace SalesWebMvc.Models
 
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department() { }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeler(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final)); 
+        }
+
     }
 }
