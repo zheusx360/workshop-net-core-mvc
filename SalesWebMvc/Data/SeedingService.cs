6132ccbc71a66ba6e63bd7308178060dac49f287
zheusx360 <zheusx360@gmail.com>
2019-03-24 21:00:11 -0300

    Seeding Service

---
+++ SalesWebMvc/Data/SeedingService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc.Models;
+using SalesWebMvc.Models.Enums;
+
+namespace SalesWebMvc.Data
+{
+    public class SeedingService
+    {
+
+        private SalesWebMvcContext _context;
+
+        public SeedingService(SalesWebMvcContext context)
+        {
+
+            _context = context;
+
+        }
+
+        public void seed()
+        {
+            if (_context.Department.Any() || _context.Seller.Any() || _context.salesRecord.Any())
+            {
+                return; //DB has been seeded 
+            }
+
+
+            Department d1 = new Department(1, "Computers");
+            Department d2 = new Department(2, "Electronics");
+            Department d3 = new Department(3, "Fashion");
+            Department d4 = new Department(4, "Books");
+
+            Seller s1 = new Seller(1, "Bob Brown", "bob@gmail.com", new DateTime(1998,4,21), 1000.0, d1);
+            Seller s2 = new Seller(2, "Maria green", "mgreen@gmail.com", new DateTime(1979,12,31), 1800.0, d2);
+            Seller s3 = new Seller(3, "Alex Grey", "alex@gmail.com", new DateTime(1988,1,15), 1900.0, d3);
+            Seller s4 = new Seller(4, "Martha Red", "martha@gmail.com", new DateTime(1993,11,30), 2500.0, d4);
+
+
+            SalesRecord r1 = new SalesRecord(1, new DateTime(2018, 09, 25), 11000.0, SaleStatus.Billed, s1);
+            SalesRecord r2 = new SalesRecord(2, new DateTime(2018, 10, 02), 15000.0, SaleStatus.Billed, s2);
+            SalesRecord r3 = new SalesRecord(3, new DateTime(2018, 11, 15), 2000.0, SaleStatus.Billed, s3);
+            SalesRecord r4 = new SalesRecord(4, new DateTime(2018, 11, 08), 1600.0, SaleStatus.Billed, s4);
+            SalesRecord r5 = new SalesRecord(5, new DateTime(2018, 12, 17), 12000.0, SaleStatus.Billed, s1);
+
+            _context.Department.AddRange(d1, d2, d3, d4);
+            _context.Seller.AddRange(s1, s2, s3, s4);
+            _context.salesRecord.AddRange(r1, r2, r3, r4, r5);
+
+
+            _context.SaveChanges();
+            
+        }
+
+
+        
+
+    }
+}
